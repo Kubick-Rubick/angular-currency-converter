f06ed70f190607e27ed7496ab0cf22c83f28afd7
Alex <prokun2012@gmail.com>
2022-12-11 05:51:37 +0200

    Update currency.service.ts

--- src/app/shared/service/currency.service.ts
+++ src/app/shared/service/currency.service.ts
@@ -24,19 +24,23 @@ export class CurrencyService{
   public getCurrenciesPromise(): any{
     return new Promise<any>((resolve, reject) => {
       if(this.currencies.length==0) {
+        //get rate and short name of currency
         this.http.get<any>('https://open.er-api.com/v6/latest/UAH').subscribe(data => {
-          for (var key in data.rates){
-            var value: number = data.rates[key];
+          for (let key in data.rates){
+            let value: number = data.rates[key];
+            // for example rate: 38.28, name: USD  
             let currency:Currency = {rate: value, full_name: '', name: key, symbol: ''};
             this.currencies.push(currency);
           }
+          // last rate update of service
           this.lastUpdate = data.time_last_update_utc;
           this.http.get<any>('https://restcountries.com/v3.1/all?fields=currencies').subscribe((data) => {
 
             data.forEach(currency => {
               let name: string = Object.keys(currency.currencies)[0]
-              var index: number = this.currencies.findIndex(element => element.name==name);
+              let index: number = this.currencies.findIndex(element => element.name==name);
               if (index!=-1)
+                // for example full_name: American dollar, symbol: $  
                 this.currencies[index] = {...this.currencies[index], full_name: currency.currencies[name].name, symbol: currency.currencies[name].symbol}
             })
           resolve(this.currencies);
